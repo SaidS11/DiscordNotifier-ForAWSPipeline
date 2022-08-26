242ebd43d0c2972a76b9caf7b57b65e3342e8e04
Said Silva <89668449+SaidS11@users.noreply.github.com>
2022-08-26 17:06:28 -0500

    Update function

--- lambda_function.py
+++ lambda_function.py
@@ -5,20 +5,19 @@ print('Loading function')
 
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
     message = event['Records'][0]['Sns']['Message']
     print("From SNS: " + message)
-    message_str=str(message)
-    if "New" in message:
-        webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1011442516266066000/ewLbjHcrp0AmpwYthQh-K4EiBtz3WEdiRr6_ZIlihuvSPH4IQSX3FG9bo2Puq363OcFg")
-        embed = DiscordEmbed(
-            title="AWS",
-            description=message,
-            color="00ff00")
-        
-        #embed.set_image(url="")
-        webhook.add_embed(embed)
-        webhook.execute()
-    else:
-        pass
-    return message
+
+    webhook = DiscordWebhook(url="replace with your webhook url")
+    embed = DiscordEmbed(
+        title="AWS",
+        description=message,
+        color="00ff00")
+
+    #Select an image if you want
+    #embed.set_image(url="")
+
+    webhook.add_embed(embed)
+    webhook.execute()
+
+    return messa
